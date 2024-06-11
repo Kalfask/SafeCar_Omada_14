6a3823d2308942701018fb5e4915f761823d0ffc
Kalfask <153239926+Kalfask@users.noreply.github.com>
2024-06-12 02:44:57 +0300

    Delete SafeCar_Omada_Omada_14 directory

--- SafeCar_Omada_Omada_14/src/Statistics.java
+++
@@ -1,9 +0,0 @@
-public class Statistics {
-	private Object _analyses;
-	private Object _reports;
-	public Screen _unnamed_Screen_;
-
-	public void findStatistics() {
-		throw new UnsupportedOperationException();
-	}
-}
